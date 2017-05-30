03fca44f84e11642f0762c698ff6dba05e1aed12
AbhiRKeesara <AbhiRKeesara@users.noreply.github.com>
2017-05-29 20:57:48 -0500

    Updated resumeBuilder.js
    made changes to location and corrected its typo.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,7 +10,7 @@
 				"email": "abhinayreddy_keesara@student.uml.edu",
 				"github": "AbhiRKeesara",
 				"twitter": "AbhiRKeesara1",
-				"Location": "Des Plained, IL."
+				"Location": "Des Plaines, IL."
 			},
 
 			"skills": ["HTML","CSS","JS","Swift","Objective-C","Python","C"],
